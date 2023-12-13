642586a786b564925318630c7e1c0c36828ffd7c
Lokesh Govindu <lokesh.govindu@hexagon.com>
2023-12-13 18:59:25 +0530

    Removed small.ico file.

--- source/resource.h
+++ source/resource.h
@@ -8,7 +8,6 @@
 #define IDM_ABOUT                       104
 #define IDM_EXIT                        105
 #define IDI_ALTTAB                      107
-#define IDI_SMALL                       108
 #define IDC_ALTTAB                      109
 #define IDR_MAINFRAME                   128
 #define IDC_TRAY_CONTEXTMENU            129
